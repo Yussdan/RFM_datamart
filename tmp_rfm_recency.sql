4dffec2420498d7ba3a7e90175a9f074279640be
Danil <Yussdan@gmail.com>
2023-07-12 09:50:07 +0600

    add recency

---
+++ tmp_rfm_recency.sql
@@ -0,0 +1,27 @@
+WITH i AS 
+(
+    SELECT vu2.id, tmp2.ord, row_number() OVER (ORDER BY COALESCE(tmp2.ord, '0001-01-01') DESC) as ran
+    FROM (
+            SELECT id, order_ts AS ord
+            FROM (
+                SELECT vu.id, order_ts, row_number() OVER (PARTITION BY vu.id ORDER BY order_ts DESC) AS ran
+                FROM view_user vu
+                LEFT JOIN view_orders vo ON vu.id = vo.user_id
+                LEFT JOIN view_orderstatuses vo2 ON vo.status = vo2.id
+                WHERE key = 'Closed'
+            ) tmp
+            WHERE tmp.ran = 1
+            ORDER BY tmp.order_ts DESC
+         ) tmp2
+    RIGHT JOIN view_user vu2 ON vu2.id = tmp2.id
+    ORDER BY tmp2.ord DESC
+)
+SELECT id as user_id, ord as last_order_dt,
+  CASE
+    WHEN ran >= 1 AND ran <= (SELECT COUNT(*) FROM i) / 5 THEN 5
+    WHEN ran > (SELECT COUNT(*) FROM i) / 5 AND ran <= 2 * (SELECT COUNT(*) FROM i) / 5 THEN 4
+    WHEN ran > 2 * (SELECT COUNT(*) FROM i) / 5 AND ran <= 3 * (SELECT COUNT(*) FROM i) / 5 THEN 3
+    WHEN ran > 3 * (SELECT COUNT(*) FROM i) / 5 AND ran <= 4 * (SELECT COUNT(*) FROM i) / 5 THEN 2
+    ELSE 1
+  END AS recency
+FROM i;
--- views.sql
+++ views.sql
@@ -10,3 +10,10 @@ create MATERIALIZED VIEW view_Products AS (
 create MATERIALIZED VIEW view_Orders AS (
 	SELECT * FROM production.orders o 
 );
+create MATERIALIZED VIEW view_user AS (
+	SELECT * FROM production.user u
+);
+create MATERIALIZED VIEW view_orderstatuslog AS (
+	SELECT * FROM production.orderstatuslog u
+); /* для перерасчета view_orders в запросе orders_view.sql /*
+
